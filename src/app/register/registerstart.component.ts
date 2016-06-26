f63be095ba29358f174aefe216ffebb4d252aa44
webdev498 <gcmobile@mail.com>
2016-06-26 13:08:12 -0600

    Email validations service

--- src/app/register/registerstart.component.ts
+++ src/app/register/registerstart.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {PasswordMinLength} from './../model/Constants';
 import {RegistrationStartInfo} from './../model/Constants';
 import {RegistrationService} from './register.service';
+import {ValidationService} from './../services/validation.service';
 import {RegistrationStart} from './../model/RegistrationStart';
 
 import {Router} from '@angular/router';
@@ -10,16 +11,18 @@ import {NgForm, Validators} from '@angular/forms';
 @Component({
   directives: [
   ],
-  providers: [RegistrationService],
+  providers: [RegistrationService, ValidationService],
   styles: [ require('./register.less') ],
   template: require('./registerstart.html')
 })
 export class RegisterStartComponent implements OnInit {
   validationMessage: string;
   rs: RegistrationStart;
+  emailValid: boolean = true;
 
   constructor(private _regService: RegistrationService,
-                private _router: Router) {
+                private _router: Router,
+                private _validator: ValidationService) {
       this.validationMessage = '';
       this.rs = new RegistrationStart();
   }
@@ -34,12 +37,25 @@ export class RegisterStartComponent implements OnInit {
   
   submitButtonState() {
       if (this.rs.email.length > 0
+          && this.emailValid
           && this.rs.password.length >= PasswordMinLength
           && this.rs.passwordConfirmation.length >= PasswordMinLength
           && this.rs.password == this.rs.passwordConfirmation)
           return false;
       else
         return true;
+  }
+
+  emailValidation() {
+      if (this.rs.email.length > 0) {
+          if (!this._validator.emailIsValid(this.rs.email)) {
+            this.emailValid = false;
+            this.validationMessage = 'Email address is invalid';
+          }
+      } else {
+          this.emailValid = true;
+          this.validationMessage = '';
+      }
   }
   
   register() {
--- src/app/services/validation.service.ts
+++ src/app/services/validation.service.ts
@@ -3,4 +3,11 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ValidationService {
     constructor() {}
+
+    emailIsValid(address: string): boolean {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(address))  
+            return true;
+        else
+            return false;
+    }
 }
