0efe03d269f1a0a274019ea72d6ceb18e623b3cf
webdev498 <gcmobile@mail.com>
2016-06-29 21:53:40 -0600

    Nationalities and registration service integration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnInit} from '@angular/core';
-import {Router, ROUTER_DIRECTIVES} from '@angular/router';
+import {Router} from '@angular/router';
 import {NgForm} from '@angular/common';
 
 import {Registration} from './../model/Registration';
-import {MaskDirective} from './../directives/mask.directive';
-import {PasswordMinLength} from './../model/Constants';
+import {RegistrationStartInfo} from './../model/Constants';
+import {UserAccount} from './../model/dto/UserAccount';
+
+import {RegistrationStart} from './../model/RegistrationStart';
+
 import {NationalityService} from './../services/nationality.service';
 import {CountryCodeService} from './../services/countrycode.service';
 import {StateService} from './../services/state.service';
@@ -12,6 +15,7 @@ import {DayService} from './../services/day.service';
 import {MonthService} from './../services/month.service';
 import {YearService} from './../services/year.service';
 import {RegistrationService} from './register.service';
+import {AuthService} from './../services/auth.service';
 
 import {CountryCode} from './../model/CountryCode';
 import {Day} from './../model/Day';
@@ -21,13 +25,12 @@ import {Month} from './../model/Month';
   selector: 'register', 
 
   directives: [
-    MaskDirective,
-    ...ROUTER_DIRECTIVES
   ],
 
   providers: [
     NationalityService, CountryCodeService, DayService,
-    MonthService, YearService, StateService, RegistrationService
+    MonthService, YearService, StateService, RegistrationService,
+    AuthService
     ],
 
   styles: [ require('./register.less'), require('./../app.less') ],
@@ -38,6 +41,7 @@ export class RegisterComponent implements OnInit {
   // Set our default values
   validationMessage: string;
   ur: Registration;
+  rs: RegistrationStart;
   countryCodes: CountryCode[];
   years: string[];
   states: string[];
@@ -52,7 +56,9 @@ export class RegisterComponent implements OnInit {
               private _monthService: MonthService,
               private _yearService: YearService,
               private _stateService: StateService,
-              private _regService: RegistrationService) {
+              private _regService: RegistrationService,
+              private _router: Router,
+              private _authService: AuthService) {
       this.validationMessage = '';
       this.ur = new Registration();
       this.countryCodes = _countryCodeService.countryCodes();
@@ -71,11 +77,42 @@ export class RegisterComponent implements OnInit {
   }
   
   submitRegistration() {
-      
+      this.validationMessage = '';
+      let userAccount = new UserAccount().setPayload(this.ur);
+      let payload = userAccount.getPayload();
+      this._regService.postRegistration(payload)
+        .subscribe(
+            response => this._handleRegistrationResponse(response),
+            error => this._handleError(error, 'There was an error during the registration process')
+        );
+  }
+
+  _handleRegistrationResponse(response) {
+    console.log(response);
+    //this._authService.saveUser(response);
+    //show confirmation and tell user to login
+    //this._router.navigateByUrl('login');
+  }
+
+  _handleError(error, message) {
+    this.validationMessage = message;
+    console.error(error);
+  }
+
+  _handleNationalityResponse(response) {
+    console.log(response);
   }
   
   ngOnInit() {
-
+    if (sessionStorage[RegistrationStartInfo] != null) {
+      this.ur = <Registration>JSON.parse(sessionStorage.getItem(RegistrationStartInfo));
+      
+      this._nationalityService.getNationalities()
+        .subscribe(
+            response => this._handleNationalityResponse(response),
+            error => this._handleError(error, 'There was an error retrieving the nationalities')
+        );
+    }
   }
 
 }
