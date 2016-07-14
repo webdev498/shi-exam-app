b0d9cf476c298b66feae0f779ac7d27b2b38e6be
webdev498 <gcmobile@mail.com>
2016-07-13 19:05:49 -0600

    Login/Logout and Registration finished

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,21 +3,27 @@ import {UserInfoKey} from './../model/Constants';
 import {User} from './../model/User';
 import {JwtHelper} from 'angular2-jwt';
 import {Injectable} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class AuthService {
     jwt = new JwtHelper();
-    constructor() {}
+    constructor(private _router: Router) {}
     
     loggedIn() {
         if (localStorage[UserTokenKey] != null) {
-            if (this.jwt.isTokenExpired(localStorage[UserTokenKey]))
+            if (this.jwt.isTokenExpired(JSON.parse(localStorage[UserTokenKey]).token))
                 return false;
                 
             return true;    
         }
         
         return false;
+    }
+
+    logout() {
+        localStorage.clear();
+        this._router.navigate['home'];
     }
     
     saveToken(token) {
@@ -30,7 +36,7 @@ export class AuthService {
     
     tokenUserInfo() {
         if (localStorage[UserTokenKey] != null) {
-        return <User>this.jwt.decodeToken(JSON.parse(localStorage[UserTokenKey]));
+        return <User>this.jwt.decodeToken(JSON.parse(localStorage[UserTokenKey]).token);
         } else {
             return null;
         }
