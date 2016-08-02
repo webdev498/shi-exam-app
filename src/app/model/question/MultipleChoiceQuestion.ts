e3c306579c212bd891df7d1b24f3c63a27d69c27
webdev498 <gcmobile@mail.com>
2016-08-01 22:06:46 -0600

    Question

--- src/app/model/question/MultipleChoiceQuestion.ts
+++ src/app/model/question/MultipleChoiceQuestion.ts
@@ -1,7 +1,9 @@
 import {Choice} from './Choice';
+import {Question} from './Question';
 
 export class MultipleChoiceQuestion {
     public choices: Choice[];
+    public question: Question;
     
     constructor() {}
                 
