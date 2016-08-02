1432b14174d55452c88a65265a05d546865be1ac
webdev498 <gcmobile@mail.com>
2016-08-02 09:53:59 -0600

    Interface fix

--- src/app/model/question/MultipleChoiceQuestion.ts
+++ src/app/model/question/MultipleChoiceQuestion.ts
@@ -1,9 +1,11 @@
 import {Choice} from './Choice';
 import {Question} from './Question';
+import {QuestionInterface} from './../interface/Question.interface';
 
-export class MultipleChoiceQuestion {
+export class MultipleChoiceQuestion implements QuestionInterface {
     public choices: Choice[];
     public question: Question;
+    public questionType: string;
     
     constructor() {}
                 
