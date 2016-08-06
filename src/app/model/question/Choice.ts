1c0d0b1f161ce54c17c8a25b9ec4b17dd644b237
webdev498 <gcmobile@mail.com>
2016-08-06 12:51:45 -0600

    Matching working!

--- src/app/model/question/Choice.ts
+++ src/app/model/question/Choice.ts
@@ -2,13 +2,16 @@ import { ChoiceInterface } from './../interface/Choice.interface';
 
 export class Choice implements ChoiceInterface {
     public selected: boolean = false;
+    public matchedchoice: Choice;
     public text: string;
     public id: string;
+    public matched: boolean = false;
 
     constructor(text: string, id: string)
      {
          this.text = text;
          this.id = id;
+         this.matched = false;
      }
                 
 }
--- src/app/questions/matching/matching.component.ts
+++ src/app/questions/matching/matching.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {Choice} from './../../model/question/Choice';
+var _ = require('lodash');
  
 @Component({ 
     selector: 'matching', 
@@ -14,10 +15,35 @@ export class Matching {
         
     }
 
-    matched(choice: Choice) {
+    dragstart(ev, id) {
+        ev.dataTransfer.setData('choice', id);
+    }
+
+    dropped(ev, id) {
+        ev.preventDefault();
+        let droppedid = ev.dataTransfer.getData('choice');
+        this._matched(id, droppedid);
+    }
+
+    dragover(ev) {
+        ev.preventDefault();
+        ev.dataTransfer.dropEffect = 'move';
+    }
+
+    _matched(id: string, droppedid: string) {
         this.choiceMatched.emit({
-            id: choice.id
-        })
+            id: id,
+            matchedid: droppedid
+        });
+
+        let choice = <Choice>this._getChoice(id);
+        let droppedChoice = <Choice>this._getChoice(droppedid);
+        choice.matchedchoice = droppedChoice;
+        droppedChoice.matched = true;
+    }
+
+    _getChoice(id) {
+        return _.find(this.choices, {id: id});
     }
     
 }
