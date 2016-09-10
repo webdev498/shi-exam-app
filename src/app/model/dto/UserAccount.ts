83a9c5f6eac9015ab9a8f5bd8fd969bfab29b479
webdev498 <gcmobile@mail.com>
2016-09-09 19:26:28 -0600

    A few fixes

--- src/app/model/dto/UserAccount.ts
+++ src/app/model/dto/UserAccount.ts
@@ -34,7 +34,7 @@ export class UserAccount extends UserRegistration{
         let phNumber = null;
         if (registration.phone.length == 10) {
             areaCode = registration.phone.substring(0,3);
-            phNumber = registration.password.substring(3,10);
+            phNumber = registration.phone.substring(3,10);
 
             let phone = new Telephone(registration.countryCode,areaCode,phNumber,null);
             phone.id = registration.phoneId;
