9876a00ceba86a3732219056f81c6bd1c9e76a7c
webdev498 <gcmobile@mail.com>
2016-10-18 21:46:09 -0600

    Register Confirm page and component

---
+++ src/app/register/registerconfirm.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'registerconfirm',
+  styles: [ require('./register.less') ],
+  template: require('./registerconfirm.html')
+})
+export class RegisterConfirmComponent {
+
+  constructor() {
+
+  }
+
+}
