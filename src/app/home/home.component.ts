4933a70dc224164ab2aa0c533e76c1b7527c406e
webdev498 <gcmobile@mail.com>
2016-11-20 07:44:27 -0700

    Basic dashboard construct

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,30 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService as CGIAuth} from './../services/auth.service';
+import {ExamService} from './../exam/exam.service';
 
 @Component({
   selector: 'home',  // <home></home>
   styles: [ require('./home.less') ],
+  providers: [ExamService],
   template: require('./home.html')
 })
 export class HomeComponent implements OnInit {
 
   constructor(private _router: Router,
-              private _cgiAuth: CGIAuth) {
+              private _cgiAuth: CGIAuth,
+              private _examService: ExamService) {
       
   }
+
+  examIconText: string;
 
   ngOnInit() {
     //navigate to login screen if not logged in
     if (!this._cgiAuth.loggedIn())
       this._router.navigate(['login']);
+
+   this.examIconText = "Take Another Test";
   }
 
 }
