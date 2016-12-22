ab5b30cc1c7e19be6dec16c90e477e95685d2e88
webdev498 <gcmobile@mail.com>
2016-12-21 22:03:39 -0700

    Agreement capture token

--- src/app/study/agreement.component.ts
+++ src/app/study/agreement.component.ts
@@ -1,12 +1,28 @@
-import {Component} from '@angular/core';
+import {Subscription } from 'rxjs';
+import {Router, ActivatedRoute} from '@angular/router';
+import {Component, OnInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'studyagreement',  
   styles: [ require('./agreement.less'), require('./../app.less') ],
   template: require('./agreement.html')
 })
-export class AgreementComponent {
-    constructor() {}
+export class AgreementComponent implements OnInit, OnDestroy {
+    constructor(private _activatedRoute: ActivatedRoute) {}
     
+    private subscription: Subscription;
     private _token: string;
+
+    ngOnInit() {
+        // subscribe to router event
+        this.subscription = this._activatedRoute.params.subscribe(
+          (param: any) => {
+            this._token = param['token'];
+          });
+    }
+
+    ngOnDestroy() {
+        // prevent memory leak by unsubscribing
+        this.subscription.unsubscribe();
+  }
 }
