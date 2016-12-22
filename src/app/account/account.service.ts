2e4e937a74903ad72bac5202f504271fd43a1cf1
webdev498 <gcmobile@mail.com>
2016-12-21 22:16:17 -0700

    api calls

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -37,12 +37,31 @@ export class AccountService {
       .catch(this.handleError);
   }
 
-  premierActivate() {
-
+  premierStudyActivate() {
+    let header = new Headers();
+    header.append(AuthHeaderKey,this._authService.getToken());
+    header.append('Content-Type','application/json');
+    return this._http.post(RootApiUrl + '/study/process', {
+      headers: header
+    })
+      .map((response: Response) => <any>response.json())
+      .catch(this.handleError);
   }
 
-  premierCancel() {
+  premierStudyCancel() {
     
+  }
+
+  premierStudyFinalize(token: string) {
+    let header = new Headers();
+    header.append(AuthHeaderKey,this._authService.getToken());
+    header.append('Content-Type','application/json');
+    let body = {token: token}
+    return this._http.post(RootApiUrl + '/study/finalize', body, {
+      headers: header
+    })
+      .map((response: Response) => <any>response.json())
+      .catch(this.handleError);
   }
 
   private handleError(error: Response) {
