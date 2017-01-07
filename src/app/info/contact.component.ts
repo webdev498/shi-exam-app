f7dcb5f35cfc51d7b1fe929dcbb4d5c3f9372e3b
webdev498 <gcmobile@mail.com>
2017-01-07 12:51:54 -0700

    info components and theme changes

---
+++ src/app/info/contact.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'contact',  
+  styles: [ require('./../app.less') ],
+  template: require('./contact.html')
+})
+export class ContactComponent {
+    
+    constructor() {}
+    
+}
