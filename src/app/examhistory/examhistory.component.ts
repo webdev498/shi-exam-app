3cd2e4731c1c7d1fa91d0ce1ef0924cf0c1a9309
webdev498 <gcmobile@mail.com>
2017-01-21 12:57:25 -0700

    Calling all results api endpoint

--- src/app/examhistory/examhistory.component.ts
+++ src/app/examhistory/examhistory.component.ts
@@ -1,23 +1,48 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthService} from './../services/auth.service';
+import {ExamService} from './../exam/exam.service';
+import {EventService} from './../services/event.service';
 
 @Component({
   selector: 'examhistory',  
   styles: [ require('./examhistory.less'), require('./../app.less') ],
+  providers: [ExamService],
   template: require('./examhistory.html')
 })
 export class ExamHistoryComponent implements OnInit {
     
     constructor(private _router: Router,
-                private _authService: AuthService) 
+                private _authService: AuthService,
+                private _examService: ExamService,
+                private _eventService: EventService) 
                 {
 
                 }
+
+    results: any;
     
     ngOnInit() {
       if (!this._authService.premierUser()) {
         this._router.navigate(['premiumupgrade']);
+        return;
       }
+
+      this._examService.allExamScores()
+            .subscribe(
+          response => {
+            this._handleAllExamResponse(response);
+          },
+          error => this._handleAllExamError(error)
+        );
+    }
+
+    _handleAllExamResponse(exams: any) {
+      console.log(exams);
+    }
+
+    _handleAllExamError(error: any) {
+      this._eventService.broadcast('error', 'There was an issue retrieving your history');
+      console.error(error);
     }
 }
