592b62efc39609a027a0c6b7b3c5873fceb6e17f
webdev498 <gcmobile@mail.com>
2017-03-07 13:30:35 -0800

    question choices

--- src/app/studyquestionchoice/studyquestionchoice.component.ts
+++ src/app/studyquestionchoice/studyquestionchoice.component.ts
@@ -1,14 +1,28 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {MultipleChoice} from './../questions/multiplechoice/multiplechoice.component';
+import {Matching} from './../questions/matching/matching.component';
+import {Grouping} from './../questions/grouping/grouping.component';
+import {FillInBlankComponent} from './../questions/fillinblank/fillinblank.component';
+import {FlashcardComponent} from './../questions/flashcard/flashcard.component';
+import {Term} from './../model/question/Term';
 
 @Component({
   selector: 'studyquestionchoice',  
   styles: [ require('./studyquestionchoice.less') ],
   template: require('./studyquestionchoice.html'),
 })
-export class StudyQuestionChoiceComponent {  
+export class StudyQuestionChoiceComponent implements OnInit {  
     constructor() {} 
 
-    start(questionType: number) {
-        
+    public currentQuestionType: string;
+    public fetching: boolean;
+
+    ngOnInit() {
+      this.fetching = true;
+    }
+
+    start(questionType: string) {
+      this.currentQuestionType = questionType;   
     }
 }
