0e261d769b40c50a5582fc25dfd5610dcc9de916
webdev498 <gcmobile@mail.com>
2017-03-20 14:19:28 -0400

    fillinblank logic

--- src/app/questions/fillinblank/fillinblank.component.ts
+++ src/app/questions/fillinblank/fillinblank.component.ts
@@ -10,4 +10,20 @@ import {Term} from './../../model/question/Term';
 export class FillInBlankComponent {
 
   @Input() term: Term;
+
+  public termInput: string;
+  public success: boolean = false;
+  public complete: boolean = false;
+
+  entered() {
+    this.success = false;
+    
+    for (let i = 0; i < this.term.translations.length; i++) {
+      if (this.term.translations[i].text == this.termInput) {
+        this.success = true;
+      }
+    }
+
+    this.complete = true;
+  }
 }
