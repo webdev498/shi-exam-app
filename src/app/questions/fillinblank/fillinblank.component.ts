9bb9e6bf16fbf4ca0bf6d7661414e6a060d234e9
webdev498 <gcmobile@mail.com>
2017-04-02 09:12:10 -0600

    Fill in blank component

--- src/app/questions/fillinblank/fillinblank.component.ts
+++ src/app/questions/fillinblank/fillinblank.component.ts
@@ -1,5 +1,7 @@
-import {Component, Input} from '@angular/core';
-import {Term} from './../../model/question/Term';
+import {Component, Input, OnInit, OnChanges, SimpleChanges} from '@angular/core';
+import {StudyTerm} from './../../model/question/StudyTerm';
+
+declare var iSpeechTTS: any;
 
 @Component({
   selector: 'fillinblank',
@@ -8,11 +10,42 @@ import {Term} from './../../model/question/Term';
 })
 export class FillInBlankComponent {
 
-  @Input() term: Term;
-
+  @Input() terms: StudyTerm[];
+  
+  public view: boolean = false;
   public termInput: string;
   public success: boolean = false;
   public complete: boolean = false;
+  public term: StudyTerm;
+
+  public enableFeedback: boolean = false;
+  public feedbackSubmitted: boolean = false;
+
+  private _tts: any;
+  private _count = 0;
+
+  ngOnInit() {
+      let audioPlayer = document.getElementById('audioPlayer');
+      this._tts = new iSpeechTTS(audioPlayer, {
+            apiKey: 'a4bf1a576382f5e3d671243e5fbbc072',
+            voice: 'usspanishfemale'
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+        if(changes['terms']) {
+          if (this.terms === undefined)
+            return;
+
+            if (this.terms.length > 0) {
+              this.term = this.terms[0];
+              this.view = true;
+
+              this.enableFeedback = false;
+              this.feedbackSubmitted = false;
+            }
+        }
+    }
 
   inputKey(e) {
     if (e.keyCode === 13)
@@ -23,11 +56,15 @@ export class FillInBlankComponent {
     this.success = false;
 
     for (let i = 0; i < this.term.translations.length; i++) {
-      if (this.term.translations[i].text == this.termInput) {
+      if (this.term.translations[i].value == this.termInput) {
         this.success = true;
       }
     }
 
     this.complete = true;
+  }
+
+  next() {
+    
   }
 }
