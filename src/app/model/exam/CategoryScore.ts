09ccae5bd3788baceeea282a346991962f3ffec9
webdev498 <gcmobile@mail.com>
2017-04-30 13:17:32 -0600

    Fixed exam complete category data

--- src/app/model/exam/CategoryScore.ts
+++ src/app/model/exam/CategoryScore.ts
@@ -8,4 +8,11 @@ export class CategoryScore {
     }
     public correct: number;
     public total: number;
+
+    get percent(): string {
+        if (this.correct === 0)
+            return '(0%)'
+
+        return `(${Math.floor((this.correct / this.total * 100)).toString()})%`;
+    }
 }
